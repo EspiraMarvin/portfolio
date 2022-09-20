10fa7eafeb46daf7ceac1d7ec6b3b956bb829851
marvin espira <espiramarvin@gmail.com>
2022-09-20 17:59:41 +0300

    feat: add updates

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Skill as SkillType } from '../typings'
+import { urlFor } from '../sanity'
 
 type Props = {
+    skill: SkillType,
     directionLeft?: boolean
 }
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ directionLeft, skill }: Props) {
   return (
-    <div className="group relative flex cursor-pointer">
+    <div className="relative flex cursor-pointer group">
         <motion.img 
          initial={{
             x: directionLeft ? -200 : 200,
@@ -15,12 +18,13 @@ export default function Skill({ directionLeft }: Props) {
          }}
          transition={{ duration: 1 }}
          whileInView={{ opacity: 1, x: 0 }}
-         src="https://icon-library.com/images/html5-icon/html5-icon-13.jpg"
-         className="rounded-full border border-gray-500 object-cover h-24 w-24 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+         viewport={{ once: true }}
+        src={urlFor(skill?.image).url()}
+         className="object-cover w-24 h-24 transition duration-300 ease-in-out border border-gray-500 rounded-full xl:h-28 xl:w-28 filter group-hover:grayscale"
         />
-        <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:w-32 xl:h-32 rounded-full z-0">
+        <div className="absolute z-0 w-24 h-24 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:opacity-80 group-hover:bg-white xl:w-28 xl:h-28">
             <div className="flex items-center justify-center h-full">
-                <p className="text-3xl font-bold text-black opacity-100">100%</p>
+                <p className="text-3xl font-bold text-black opacity-100">{skill?.progress}%</p>
             </div>
         </div>
     </div>
