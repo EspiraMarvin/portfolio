95fd76fdfac61ba56ed7702b1359fb4c142538bf
marvin espira <espiramarvin@gmail.com>
2022-09-20 17:53:52 +0300

    feat: add updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { Social } from '../typings'
 
-type Props = {}
+type Props = {
+    socials: Social[]
+}
 
-export default function Header({}: Props) {
+export default function Header({socials }: Props) {
   return (
-    <header className="sticky top-0 p-5 flex items-start xl:items-center justify-between max-w-7xl mx-auto z-20 ">
+    <header className="sticky top-0 z-20 flex items-start justify-between p-5 mx-auto xl:items-center max-w-7xl ">
         <motion.div 
         initial={{
             x: -500,
@@ -22,54 +26,41 @@ export default function Header({}: Props) {
         className="flex flex-row items-center"
         >
             {/* social icons */}
-            <SocialIcon 
-                url="https://twitter.com/marvin_espira" 
-                fgColor="gray"
-                bgColor="transparent"
-                target="_blank"
-            />
-            <SocialIcon 
-                url="https://twitter.com/marvin_espira" 
-                fgColor="gray"
-                bgColor="transparent"
-                target="_blank"
-            />
-            <SocialIcon 
-                url="https://github.com/EspiraMarvin" 
-                fgColor="gray"
-                bgColor="transparent"
-                target="_blank"
-            />
-           <SocialIcon 
-                url="https://www.linkedin.com/in/marvin-espira/" 
-                fgColor="gray"
-                bgColor="transparent"
-                target="_blank"
-            />
+            { socials.map((social, i) => (
+                <SocialIcon 
+                    key={social._id}
+                    url={social.url} 
+                    fgColor="gray"
+                    bgColor="transparent"
+                    target="_blank"
+                />
+            ))}
 
         </motion.div>
 
-        <motion.div 
-         initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5
-        }}
-        animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1
-        }}
-        transition={{ duration: 1 }}
-        className="flex flex-row items-center">
-            <SocialIcon 
-                className="cursor-pointer"
-                network="email"
-                fgColor="gray"
-                bgColor='transparent'
-            />
-            <p className="uppercase hidden md:inline-flex text-gray-400 text-sm">Get In Touch</p>
-        </motion.div>
+        <Link href="#contact">
+            <motion.div 
+            initial={{
+                x: 500,
+                opacity: 0,
+                scale: 0.5
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1
+            }}
+            transition={{ duration: 1 }}
+            className="flex flex-row items-center cursor-pointer">
+                <SocialIcon 
+                    network="email"
+                    fgColor="gray"
+                    bgColor='transparent'
+                />
+                <p className="hidden text-sm text-gray-400 uppercase md:inline-flex">Get In Touch</p>
+            </motion.div>
+        </Link>
+
     </header>
   )
 }
