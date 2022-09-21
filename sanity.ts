0a19d822ac8139ebbe646dad1e1c231ec645ce0f
marvin espira <espiramarvin@gmail.com>
2022-09-21 13:51:52 +0300

    add updates

--- sanity.ts
+++ sanity.ts
@@ -3,9 +3,9 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: "ozf66dei", 
-    // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    apiVersion: "2021-03-25",
+    // projectId: "ozf66dei", 
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    // apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
 }
 
