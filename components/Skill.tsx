9ccd6d44bbca0e85dd49ed9187140845443bf15d
marvin espira <espiramarvin@gmail.com>
2022-09-22 09:22:51 +0300

    add updates

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,7 +20,7 @@ export default function Skill({ directionLeft, skill }: Props) {
          whileInView={{ opacity: 1, x: 0 }}
          viewport={{ once: true }}
         src={urlFor(skill?.image).url()}
-         className="object-cover w-24 h-24 transition duration-300 ease-in-out border border-gray-500 rounded-full xl:h-28 xl:w-28 filter group-hover:grayscale"
+         className="object-contain w-24 h-24 transition duration-300 ease-in-out border border-gray-500 rounded-full xl:h-28 xl:w-28 filter group-hover:grayscale"
         />
         <div className="absolute z-0 w-24 h-24 transition duration-300 ease-in-out rounded-full opacity-0 group-hover:opacity-80 group-hover:bg-white xl:w-28 xl:h-28">
             <div className="flex items-center justify-center h-full">
--- sanity.ts
+++ sanity.ts
@@ -6,6 +6,7 @@ export const config = {
     projectId: "ozf66dei", 
     // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     // apiVersion: "2021-03-25",
+    apiVersion: "v1",
     useCdn: process.env.NODE_ENV === "production",
 }
 
