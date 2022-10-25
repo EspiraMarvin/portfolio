44ecd38d0b0f6136aa05eb1feaa31beab113c4ab
marvin espira <espiramarvin@gmail.com>
2022-10-25 18:57:59 +0300

    feat: add RESUME

---
+++ components/Articles.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {}
+
+export default function Articles({}: Props) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
+      Articles
+    </div>
+  )
+}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,67 +1,73 @@
-import React, { forwardRef } from 'react'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import BackgroundCircles from './BackgroundCircles'
-import Image from 'next/image'
-import Link from 'next/link'
-import { PageInfo } from '../typings'
-import { urlFor } from '../sanity'
+import React, { forwardRef } from "react"
+import { useTypewriter, Cursor } from "react-simple-typewriter"
+import BackgroundCircles from "./BackgroundCircles"
+import Image from "next/image"
+import Link from "next/link"
+import { PageInfo } from "../typings"
+import { urlFor } from "../sanity"
 
 type Props = {
-    pageInfo: PageInfo | null | undefined,
+  pageInfo: PageInfo | null | undefined
 }
 
 export default forwardRef(function Hero({ pageInfo }: Props, ref: any) {
-    const [text, count] = useTypewriter({
-        words: [
-            `Hi, I'm ${pageInfo?.name}`,
-            "React, Nextjs, TS, JS..",
-            "Nodejs, Vuejs, R.Native"
-        ],
-        loop: true,
-        delaySpeed: 2000
-       })
+  const [text, count] = useTypewriter({
+    words: [
+      `Hi, I'm ${pageInfo?.name}`,
+      "React, Nextjs, TS, JS..",
+      "Nodejs, Vuejs, R.Native",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  })
   return (
-    <div ref={ref} className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center">
-        <BackgroundCircles />
-            <Image
-                src={urlFor(pageInfo?.heroImage).url()}  
-                className="object-cover w-32 h-32 rounded-full"
-                width={130}
-                height={130}
-                alt="profile" 
-            />
-        <div className="z-30">
-            <h2 className="text-sm uppercase text-gray-500 tracking-[15px]">{pageInfo?.role}</h2>
-            <h1 className="px-10 mr-3 text-3xl font-bold md:text-5xl lg:text-6xl">
-                <span className="mr-3">{text}</span>
-                <Cursor cursorColor="#0EA5E9" />
-            </h1>        
+    <div
+      ref={ref}
+      className="flex flex-col items-center justify-center h-screen space-y-8 overflow-hidden text-center"
+    >
+      <BackgroundCircles />
+      <Image
+        src={urlFor(pageInfo?.heroImage).url()}
+        className="object-cover w-32 h-32 rounded-full"
+        width={130}
+        height={130}
+        alt="profile"
+      />
+      <div className="z-30">
+        <h2 className="text-sm uppercase text-gray-500 tracking-[15px]">
+          {pageInfo?.role}
+        </h2>
+        <h1 className="px-10 mr-3 text-3xl font-bold md:text-5xl lg:text-6xl">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#0EA5E9" />
+        </h1>
 
-            <div className="pt-5">
-                <Link href="#about">
-                    <button className="heroButton">About</button>
-                </Link>
-                <Link href="#skills">
-                    <button className="heroButton">Skills</button>
-                </Link>
-                <Link href="#experience">
-                    <button className="heroButton">Experience</button>
-                </Link>
-                <Link href="#projects">
-                    <button className="heroButton">Projects</button>
-                </Link>
-            </div>
-
-            <div className="hidden pt-5">
-                <Link href="#about">
-                    <button className="heroButton">CV</button>
-                </Link>
-                <Link href="#experience">
-                    <button className="heroButton">Articles</button>
-                </Link>
-            </div>
-
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
         </div>
+
+        <div className="pt-5">
+          <Link href="https://docs.google.com/document/d/1IGgFXJgHQhfBhgjT9CEE0Jc_rHrjjuR4W23MaJbm-aU/edit#heading=h.x8fm1uorkbaw">
+            <a target="_blank">
+              <button className="heroButton">RESUME</button>
+            </a>
+          </Link>
+          <Link href="#experience" className="hidden">
+            <button className="hidden heroButton">Articles</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 })
