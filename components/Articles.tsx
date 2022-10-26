41ed0aa065c42da24fbdb411b7354cd6bfdc9a54
marvin espira <espiramarvin@gmail.com>
2022-10-26 08:14:51 +0300

    feat add articles updates

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -7,22 +7,27 @@ export default function Articles({}: Props) {
   const articles = [
     {
       id: 1,
+      name: "React Components Overview.",
       link: "https://espiramarvin.hashnode.dev/react-components",
     },
     {
       id: 2,
+      name: "Quasar Vuejs Google authentication with firebase.",
       link: "https://www.section.io/engineering-education/quasar-googleauth-with-firebase",
     },
     {
       id: 3,
+      name: "Getting started with Quasar cross-platform Framework for Vuejs.",
       link: "https://www.section.io/engineering-education/getting-started-with-quasar-framework",
     },
     {
       id: 4,
+      name: "Vuejs CRUD application with localstorage.",
       link: "https://www.section.io/engineering-education/vue2-crud-app-with-localstorage",
     },
     {
       id: 5,
+      name: "Vuejs V-model support without the need for value prop.",
       link: "https://espiramarvin.hashnode.dev/vuejs-v-model-support-without-need-for-value-prop",
     },
   ]
@@ -38,13 +43,12 @@ export default function Articles({}: Props) {
       <div className="flex flex-col items-start justify-center h-screen px-4 overflow-hidden">
         {articles.map((article) => (
           <div
-            className="flex items-center italic border-b border-white h-14 md:h-14 md:tracking-wide shadow-3xl"
+            className="flex items-center italic border-b border-white hover:border-b-blue-300 h-14 md:h-14 md:tracking-wide shadow-3xl"
             key={article.id}
           >
-            {/* <div className="flex items-center justify-start hover:text-blue-300"> */}
-            <div className="hover:text-blue-300">
+            <div className="hover:text-blue-300 ">
               <Link href={article.link}>
-                <a target="_blank">{article.link}</a>
+                <a target="_blank">{article.name}</a>
               </Link>
             </div>
           </div>
